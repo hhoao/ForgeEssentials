3bbf05669711b49572a3b7617d2d997cac0cf818
Spacebuilder2020 <spacebuilder2020@users.noreply.github.com>
2024-08-09 12:18:43 -0600

    Fix Wrapper crash on client This is still an unsupported configuration, but I want FE be able to loaded in single player like it was in the past.

--- src/main/java/com/forgeessentials/core/preloader/mixin/network/MixinSimpleChannelHandlerWrapper.java
+++ src/main/java/com/forgeessentials/core/preloader/mixin/network/MixinSimpleChannelHandlerWrapper.java
@@ -1,6 +1,8 @@
 package com.forgeessentials.core.preloader.mixin.network;
 
+import net.minecraft.client.network.NetHandlerPlayClient;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.network.NetHandlerPlayServer;
 
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -28,6 +30,10 @@ public abstract class MixinSimpleChannelHandlerWrapper<REQ extends IMessage, REP
 
     @Redirect(method = "channelRead0(Lio/netty/channel/ChannelHandlerContext;Lcpw/mods/fml/common/network/simpleimpl/IMessage;)V", at = @At(value = "INVOKE", target = "Lcpw/mods/fml/common/network/simpleimpl/IMessageHandler;onMessage(Lcpw/mods/fml/common/network/simpleimpl/IMessage;Lcpw/mods/fml/common/network/simpleimpl/MessageContext;)Lcpw/mods/fml/common/network/simpleimpl/IMessage;", remap = false), remap = false)
     private REPLY redirectNetworkHandler(IMessageHandler<?, ?> iMessageHandler, REQ message, MessageContext ctx) {
+
+        if (ctx.netHandler instanceof NetHandlerPlayClient) {
+            return messageHandler.onMessage(message, ctx);
+        }
 
         EntityPlayerMP player = ctx.getServerHandler().playerEntity;
         if (ctx.side == Side.CLIENT || !ModuleAuth.isEnabled() || ModuleAuth.isAuthenticated(player) || ModuleAuth.isAllowedMethod(message)) {
